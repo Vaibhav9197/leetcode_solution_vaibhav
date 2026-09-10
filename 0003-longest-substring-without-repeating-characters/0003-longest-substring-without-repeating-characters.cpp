3a40e4c0ecc9761bcf522fa90b9a310dfe9dbe78
vaibhav mishra <140011199+Vaibhav9197@users.noreply.github.com>
2026-09-10 15:54:41 +0530

    Time: 17 ms (60.63%), Space: 18.8 MB (61.88%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,19 +4,19 @@ public:
         int head =-1; int tail =0;
         int n = s.length();
         int ans = 0;
-        map<char,int>mp;
+        int mp[128]={0};
 
         while(tail<n){
-            while(head+1<n && mp[s[head+1]]==0){
+            while(head+1<n && mp[(unsigned char)s[head+1]]==0){
                 head++;
-                mp[s[head]]++;
+                mp[(unsigned char)s[head]]++;
             }
             ans = max(ans, head-tail+1);
 
             if(tail>head){
                 tail = head-1;
             }
-            mp[s[tail]]--;
+            mp[(unsigned char)s[tail]]--;
             tail++;
         }
         return ans;
