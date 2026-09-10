59bbdb36da2eda8b597031e307d40115f49fdad2
vaibhav mishra <140011199+Vaibhav9197@users.noreply.github.com>
2026-09-10 13:19:47 +0530

    Time: 3 ms (67.23%), Space: 10.7 MB (93.3%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        map<char,int>mp;
+        //map<char,int>mp;
+        int mp[26]={0};
         int head=-1, tail =0;
         int n = s.length();
         int maxi = INT_MIN;
         int ans =0;
         while(tail<n){
-            while(head+1<n && head+2-tail - max(maxi,mp[s[head+1]]+1) <=k){
+            while(head+1<n && head+2-tail - max(maxi,mp[s[head+1]-'A']+1) <=k){
                 head++;
-                mp[s[head]]++;
-                maxi = max(maxi, mp[s[head]]);
+                mp[s[head]-'A']++;
+                maxi = max(maxi, mp[s[head]-'A']);
             }
             ans = max(ans,head-tail+1);
-            mp[s[tail]]--;
+            mp[s[tail]-'A']--;
             tail++;
             if(head < tail-1) head = tail-1;
         }
