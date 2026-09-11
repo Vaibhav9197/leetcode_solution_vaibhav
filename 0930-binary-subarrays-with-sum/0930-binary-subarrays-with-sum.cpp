f52712c465a96598f68a71c59eb4eeb2e6859502
vaibhav mishra <140011199+Vaibhav9197@users.noreply.github.com>
2026-09-11 18:04:41 +0530

    Time: 884 ms (5.14%), Space: 32.7 MB (62.55%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        int head =-1; int tail =0;
+        int trail =0;
+        int ans =0; int sum =0;
+
+        while(tail<n){
+            while(head+1<n && (sum<goal || nums[head+1]==0)){
+                head++;
+                if(nums[head]==1)trail =0;
+                else trail++;
+                sum+=nums[head];
+            }
+            if(sum==goal) ans+=min(trail,head-tail)+1;
+            cout<<tail<<" "<<ans<<endl;
+            if(nums[tail]==1)sum--;
+            tail++;
+        }
+        return ans;
+    }
+};
